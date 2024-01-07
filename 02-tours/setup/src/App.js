80f4a31b3fdb2ea62ae3bab8b860e9a31da977b6
manasjhamj <jhamanas939@gmail.com>
2024-01-07 19:28:16 +0530

    Revert "complete tours project setup"
    This reverts commit ee29833394af4a33fe4e8655373d04e4705cb6f7.

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,39 +5,7 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  const fetchTours = async () => {
-    setLoading(true);
-
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      setLoading(false);
-      setTours(tours);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-
-    }
-  };
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    )
-  }
-  return <main>
-    <Tours tours={tours} />
-  </main>
+  return <h2>Tours Project Setup</h2>
 }
 
 export default App
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = ({ tours }) => {
-  return <section>
-    <div className="title">
-      <h2>our tours</h2>
-      <div className="underline"></div>
-    </div>
-    <div>
-      {tours.map((tour) => {
-        return <Tour key={tour.id} {...tour}></Tour>;
-      })}
-    </div>
-  </section>;
+const Tours = () => {
+  return <h2>tours component</h2>;
 };
 
 export default Tours;
